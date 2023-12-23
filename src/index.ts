5b32a8863f58fa0853ec6f83225e7d2ef98d3b57
Meng Zhou <Meng.Zhou@aer.ca>
2023-12-23 10:41:05 -0700

    add applicaiton insight

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from 'express'
 
+const appInsights = require('applicationinsights')
+
 const app = express()
+appInsights.setup('6594dca3-9bf1-4f07-8610-a4c1fbdb9b34').start()
+const client = appInsights.defaultClient
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Changed deployment name version 3!')
+  client.trackEvent({
+    name: '/',
+    properties: { customProperty: 'customer visits home page' },
+  })
+
+  res.send('Changed deployment name version 4!')
 })
 
 const port = process.env.PORT || 3000
