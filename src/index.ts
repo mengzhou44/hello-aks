2ed5deca4ad4c461a67ed1edf691548396eae403
Meng Zhou <Meng.Zhou@aer.ca>
2023-12-23 20:02:12 -0700

    replace with @azure/identity

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
-import msRestAzure from 'ms-rest-azure';
+import { DefaultAzureCredential } from '@azure/identity';
 import logger from './logger';
 import sql from 'mssql';
 
@@ -30,27 +30,14 @@ if (process.env.NODE_ENV === 'local') {
     authentication: {
       type: 'azure-active-directory-msi-app-service',
       options: {
-        tokenProvider: (callback: any) => {
-          logger.log('step1', '');
-          msRestAzure.loginWithAppServiceMSI({ resource: 'https://database.windows.net/' }, (err: any, credentials: any) => {
-            logger.log('step2', '');
-            if (err) {
-              logger.log('tokenProvider err', JSON.stringify(err));
-              callback(err, null);
-            } else {
-              logger.log('step3', '');
-              credentials.getToken((tokenError: any, tokenResponse: any) => {
-                if (tokenError) {
-                  logger.log('tokenProvider token error', JSON.stringify(tokenError));
-                  callback(tokenError, null);
-                } else {
-                  logger.log('tokenProvider success',  tokenResponse.accessToken);
-                  callback(null, tokenResponse.accessToken);
-                }
-              });
-            }
-            logger.log('step4', '');
-          });
+        tokenProvider: async (callback: any) => {
+          try {
+            const credential = new DefaultAzureCredential();
+            const tokenResponse = await credential.getToken('https://database.windows.net/.default');
+            callback(null, tokenResponse.token);
+          } catch (error) {
+            callback(error, null);
+          }
         },
       },
     },
@@ -59,19 +46,17 @@ if (process.env.NODE_ENV === 'local') {
 
 app.get('/', (req: Request, res: Response) => {
   logger.log('/', 'customer visits home page');
-
   res.send('Changed deployment name version 6!');
 });
 
 app.get('/users', async (req, res) => {
   let pool: sql.ConnectionPool | undefined;
- 
+
   try {
-    pool = await sql.connect(config);  
+    pool = await sql.connect(config);
     const result = await pool.query`SELECT Id, Name, Active FROM Users`;
     res.send(result.recordset);
   } catch (err: any) {
-    
     res.status(500).send('Internal Server Error');
   } finally {
     if (pool) {
