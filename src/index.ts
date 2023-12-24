51725451dcac1cf02675b7f59b7ead5f085b79f0
Meng Zhou <Meng.Zhou@aer.ca>
2023-12-23 20:08:07 -0700

    display error

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv';
-import express, { Request, Response } from 'express';
-import { DefaultAzureCredential } from '@azure/identity';
-import logger from './logger';
-import sql from 'mssql';
+import dotenv from 'dotenv'
+import express, { Request, Response } from 'express'
+import { DefaultAzureCredential } from '@azure/identity'
+import logger from './logger'
+import sql from 'mssql'
 
-dotenv.config();
-const app = express();
+dotenv.config()
+const app = express()
 
 const config = {
   server: 'meng-db-server.database.windows.net',
@@ -13,7 +13,7 @@ const config = {
   options: {
     encrypt: true,
   },
-};
+}
 
 if (process.env.NODE_ENV === 'local') {
   Object.assign(config, {
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'local') {
         password: process.env.DB_PASSWORD,
       },
     },
-  });
+  })
 } else {
   Object.assign(config, {
     authentication: {
@@ -32,41 +32,43 @@ if (process.env.NODE_ENV === 'local') {
       options: {
         tokenProvider: async (callback: any) => {
           try {
-            const credential = new DefaultAzureCredential();
-            const tokenResponse = await credential.getToken('https://database.windows.net/.default');
-            callback(null, tokenResponse.token);
+            const credential = new DefaultAzureCredential()
+            const tokenResponse = await credential.getToken(
+              'https://database.windows.net/.default'
+            )
+            callback(null, tokenResponse.token)
           } catch (error) {
-            callback(error, null);
+            callback(error, null)
           }
         },
       },
     },
-  });
+  })
 }
 
 app.get('/', (req: Request, res: Response) => {
-  logger.log('/', 'customer visits home page');
-  res.send('Changed deployment name version 6!');
-});
+  logger.log('/', 'customer visits home page')
+  res.send('Changed deployment name version 6!')
+})
 
 app.get('/users', async (req, res) => {
-  let pool: sql.ConnectionPool | undefined;
+  let pool: sql.ConnectionPool | undefined
 
   try {
-    pool = await sql.connect(config);
-    const result = await pool.query`SELECT Id, Name, Active FROM Users`;
-    res.send(result.recordset);
+    pool = await sql.connect(config)
+    const result = await pool.query`SELECT Id, Name, Active FROM Users`
+    res.send(result.recordset)
   } catch (err: any) {
-    res.status(500).send('Internal Server Error');
+    res.status(500).send(`Internal Server Error ${JSON.stringify(err)} `)
   } finally {
     if (pool) {
-      await pool.close();
+      await pool.close()
     }
   }
-});
+})
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+  console.log(`Server is listening on port ${port}`)
+})
